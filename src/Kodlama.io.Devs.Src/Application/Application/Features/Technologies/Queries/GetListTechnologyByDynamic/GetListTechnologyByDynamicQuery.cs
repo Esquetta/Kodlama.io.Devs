4f2bf261faf87564c95316346f9b213ba4ac25fe
esquetta <esquetta@gmail.com>
2022-09-09 16:28:13 +0300

    Get List by  dynamic query added

---
+++ src/Kodlama.io.Devs.Src/Application/Application/Features/Technologies/Queries/GetListTechnologyByDynamic/GetListTechnologyByDynamicQuery.cs
@@ -0,0 +1,49 @@
+﻿using Application.Features.Technologies.Models;
+using Application.Features.Technologies.Queries.GetlListTechonlogy;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Technologies.Queries.GetListTechnologyByDynamic
+{
+    public class GetListTechnologyByDynamicQuery: IRequest<TechonlogyListModel>
+    {
+        public Dynamic Dynamic { get; set; }
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListTechnologyByDynamicQueryHandler : IRequestHandler<GetListTechnologyByDynamicQuery, TechonlogyListModel>
+        {
+            private readonly IMapper mapper;
+            private readonly ITechnologyRepository technologyRepository;
+            public GetListTechnologyByDynamicQueryHandler(IMapper mapper, ITechnologyRepository technologyRepository)
+            {
+                this.mapper = mapper;
+                this.technologyRepository = technologyRepository;
+            }
+
+            public async Task<TechonlogyListModel> Handle(GetListTechnologyByDynamicQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Technology> techology = await technologyRepository.
+                      GetListByDynamicAsync(request.Dynamic,
+                      include: x => x.Include(x => x.Language),
+                      index: request.PageRequest.Page,
+                      size: request.PageRequest.PageSize);
+
+                TechonlogyListModel techonlogyListModel = mapper.Map<TechonlogyListModel>(techology);
+
+                return techonlogyListModel;
+
+            }
+        }
+    }
+}
