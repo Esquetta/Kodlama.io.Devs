52ec5369bbd89134d0c11040615d0edb74b906a2
esquetta <esquetta@gmail.com>
2022-09-09 16:35:01 +0300

    Controller actions  added.

---
+++ src/Kodlama.io.Devs.Src/WebAPI/WebAPI/Controllers/TechnologyController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.Technologies.Models;
+using Application.Features.Technologies.Queries.GetListTechnologyByDynamic;
+using Application.Features.Technologies.Queries.GetlListTechonlogy;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TechnologyController : BaseController
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListTechologyQuery getListTechologyQuery = new GetListTechologyQuery { PageRequest = pageRequest };
+
+            TechonlogyListModel result = await Mediator.Send(getListTechologyQuery);
+
+            return Ok(result);
+
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListTechnologyByDynamicQuery getListTechnologyByDynamicQuery = new GetListTechnologyByDynamicQuery { PageRequest = pageRequest ,Dynamic = dynamic};
+
+            TechonlogyListModel result = await Mediator.Send(getListTechnologyByDynamicQuery);
+
+            return Ok(result);
+
+
+        }
+    }
+}
