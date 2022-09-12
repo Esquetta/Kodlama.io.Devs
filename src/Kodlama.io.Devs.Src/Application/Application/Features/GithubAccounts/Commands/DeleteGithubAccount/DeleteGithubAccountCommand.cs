e554c423b639f9ce1b3b6b3428621a7eac30ce7b
esquetta <esquetta@gmail.com>
2022-09-12 20:59:35 +0300

    Delete GithubAccount command finished.

---
+++ src/Kodlama.io.Devs.Src/Application/Application/Features/GithubAccounts/Commands/DeleteGithubAccount/DeleteGithubAccountCommand.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.GithubAccounts.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.GithubAccounts.Commands.DeleteGithubAccount
+{
+    public class DeleteGithubAccountCommand : IRequest<DeletedGithubAccountDto>
+    {
+        public int Id { get; set; }
+        public class DeleteGithubAccountCommandHamdler : IRequestHandler<DeleteGithubAccountCommand, DeletedGithubAccountDto>
+        {
+            private readonly IGithubAccountRepository githubAccountRepository;
+            private readonly IMapper mapper;
+            public DeleteGithubAccountCommandHamdler(IGithubAccountRepository githubAccountRepository, IMapper mapper)
+            {
+                this.mapper = mapper;
+                this.githubAccountRepository = githubAccountRepository;
+            }
+
+            public async Task<DeletedGithubAccountDto> Handle(DeleteGithubAccountCommand request, CancellationToken cancellationToken)
+            {
+
+                GithubAccount deletedAccount = await githubAccountRepository.GetAsync(x => x.Id == request.Id);
+
+                DeletedGithubAccountDto deletedGithubAccountDto = mapper.Map<DeletedGithubAccountDto>(deletedAccount);
+
+                return deletedGithubAccountDto;
+            }
+        }
+
+    }
+}
---
+++ src/Kodlama.io.Devs.Src/Application/Application/Features/GithubAccounts/Dtos/DeletedGithubAccountDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.GithubAccounts.Dtos
+{
+    
+    public class DeletedGithubAccountDto
+    {
+        public int Id { get; set; }
+        public string AccountLink { get; set; }
+    }
+}
