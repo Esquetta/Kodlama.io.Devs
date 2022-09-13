7d60493022c43a73adae70287d5fe9faacc534f5
esquetta <esquetta@gmail.com>
2022-09-13 18:56:09 +0300

    GithubAccount controller actions completed.

--- src/Kodlama.io.Devs.Src/WebAPI/WebAPI/Controllers/GithubAccountController.cs
+++ src/Kodlama.io.Devs.Src/WebAPI/WebAPI/Controllers/GithubAccountController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.GithubAccounts.Commands.CreateGithubAccount;
+using Application.Features.GithubAccounts.Commands.DeleteGithubAccount;
+using Application.Features.GithubAccounts.Commands.UpdateGithubAccount;
 using Application.Features.GithubAccounts.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -7,23 +9,31 @@ namespace WebAPI.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
+    [Authorize]
 
     public class GithubAccountController : BaseController
-    {
-
-        private readonly IHttpContextAccessor httpContextAccessor;
-        public GithubAccountController(IHttpContextAccessor httpContextAccessor)
-        {
-            this.httpContextAccessor = httpContextAccessor;
-        }
-        
+    {      
         [HttpPost]
-        [Authorize, AllowAnonymous]
         public async Task<IActionResult> Add([FromBody] CreateGithubAccountCommand createGithubAccountCommand)
         {
             CreatedGithibAccountDto createdGithibAccountDto = await Mediator.Send(createGithubAccountCommand);
 
             return Ok(createdGithibAccountDto);
         }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateGithubAccountCommand updateGithubAccountCommand)
+        {
+            UpdatedGithubAccountDto  updatedGithubAccountDto = await Mediator.Send(updateGithubAccountCommand);
+
+            return Ok(updatedGithubAccountDto);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteGithubAccountCommand deleteGithubAccountCommand)
+        {
+            DeletedGithubAccountDto deletedGithubAccountDto = await Mediator.Send(deleteGithubAccountCommand);
+
+            return Ok(deletedGithubAccountDto);
+        }
+
     }
 }
