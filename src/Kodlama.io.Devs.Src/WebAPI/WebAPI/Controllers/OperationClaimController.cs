4d345fc32631e9d81bd919d97c40ef4c9bfe7b19
Esquetta <esquetta@gmail.com>
2022-10-06 19:56:13 +0300

    Operation Claim added.

---
+++ src/Kodlama.io.Devs.Src/WebAPI/WebAPI/Controllers/OperationClaimController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.OperationClaims.Commands.CreateOperationClaim;
+using Application.Features.OperationClaims.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationClaimController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateOperationClaimCommand createOperationClaimCommand)
+        {
+            CreatedOperationClaimDto result = await Mediator.Send(createOperationClaimCommand);
+
+            return Created("",result);
+        }
+    }
+}
