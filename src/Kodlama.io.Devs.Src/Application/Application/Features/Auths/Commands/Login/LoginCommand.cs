f7c9facfe0e394dc6087d334f0a7168be32a984f
Esquetta <esquetta@gmail.com>
2022-10-06 14:57:39 +0300

    File renaming and starting to LoginCommandHandler.

---
+++ src/Kodlama.io.Devs.Src/Application/Application/Features/Auths/Commands/Login/LoginCommand.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Developers.Dtos;
+using Core.Security.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Commands.Login
+{
+    internal class LoginCommand:IRequest<LoggedUserDto>
+    {
+        public UserForLoginDto UserForLoginDto { get; set; }
+
+        public class LoginCommandHandler
+
+    }
+}
