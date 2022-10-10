4d6d5863cc6fe4c7376f1ba6d579c39a375aad89
Esquetta <esquetta@gmail.com>
2022-10-10 19:45:45 +0300

    GetListUserOperationClaimByUserIdQuery,models and dtos added.

---
+++ src/Kodlama.io.Devs.Src/Application/Application/Features/UserOperationClaims/Models/UserOperationClaimListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.UserOperationClaims.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserOperationClaims.Models
+{
+    public class UserOperationClaimListViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserOperationsClaimsListDto[] Items { get; set; }
+    }
+}
---
+++ src/Kodlama.io.Devs.Src/Application/Application/Features/UserOperationClaims/Queries/GetListUserOperationClaimByUserId/GetListUserOperationClaimByUserIdQuery.cs
@@ -0,0 +1,36 @@
+﻿using Application.Features.UserOperationClaims.Models;
+using Application.Features.UserOperationClaims.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserOperationClaims.Queries.GetListUserOperationClaimByUserId
+{
+    public class GetListUserOperationClaimByUserIdQuery : IRequest<UserOperationClaimListViewModel>
+    {
+        public int UserId { get; set; }
+
+        public class GetListUserOperationClaimByUserIdQueryHandler : IRequestHandler<GetListUserOperationClaimByUserIdQuery, UserOperationClaimListViewModel>
+        {
+            private readonly IUserOperationClaimRepository userOperationClaimRepository;
+            private readonly IMapper mapper;
+            private readonly UserOperationClaimBusinessRules userOperationClaimBusinessRules;
+            public GetListUserOperationClaimByUserIdQueryHandler(IUserOperationClaimRepository userOperationClaimRepository, IMapper mapper, UserOperationClaimBusinessRules userOperationClaimBusinessRules)
+            {
+                this.mapper = mapper;
+                this.userOperationClaimBusinessRules = userOperationClaimBusinessRules;
+                this.userOperationClaimRepository = userOperationClaimRepository;
+            }
+
+            public async Task<UserOperationClaimListViewModel> Handle(GetListUserOperationClaimByUserIdQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
